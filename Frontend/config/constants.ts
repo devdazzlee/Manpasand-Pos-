26a23e680f24b8c8e8579886f5369201af06ddd7
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2025-08-01 15:20:55 +0500

    Update

--- Frontend/config/constants.ts
+++ Frontend/config/constants.ts
@@ -1,3 +1,3 @@
-// export const API_BASE = "https://manpasand-pos-beta.vercel.app/api/v1";
+export const API_BASE = "https://manpasand-pos-beta.vercel.app/api/v1";
 
-export const API_BASE = "http://localhost:7000/api/v1";
+// export const API_BASE = "http://localhost:7000/api/v1";
